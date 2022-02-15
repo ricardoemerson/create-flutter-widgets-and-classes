e31ca8d318da847eb8fcf9e3df032ac4bc0ed2bf
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-02-15 17:29:57 -0300

    Fix getxFeatureRoutes when user component name with space in the name.

--- src/templates/getxFeatureRoutes.ts
+++ src/templates/getxFeatureRoutes.ts
@@ -5,15 +5,15 @@ import pascalCase from './shared/functions/pascal-case';
 export default ({ componentName, getxViewsSuffix, featurePath }: CreateComponent) => (
 `import 'package:get/get.dart';
 
-import '../modules${ featurePath }/${ componentName }_binding.dart';
-import '../modules${ featurePath }/${ componentName }_page.dart';
+import '../modules${ featurePath }/${ snakeCase(componentName) }_binding.dart';
+import '../modules${ featurePath }/${ snakeCase(componentName) }_page.dart';
 
 class ${ pascalCase(componentName) }Routes {
   ${ pascalCase(componentName) }Routes._();
 
   static final routes = [
     GetPage(
-      name: '${ featurePath }',
+      name: '${ featurePath!.split('/').map(route => kebabCase(route)).join('/') }',
       page: ${ pascalCase(componentName) }${ getxViewsSuffix }.new,
       binding: ${ pascalCase(componentName) }Binding(),
     ),
